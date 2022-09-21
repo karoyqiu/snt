14448db281f6aeac2acfb2df5d9c4bb9abdaa239
Roy QIU <karoyqiu@gmail.com>
2022-09-21 11:59:05 +0800

    添加空文件。

---
+++ src/sntd/main.cpp
@@ -0,0 +1,32 @@
+﻿/*! ***********************************************************************************************
+ *
+ * \file        main.cpp
+ * \brief       sntd 主源文件。
+ *
+ * \version     0.1
+ * \date        2022-9-21
+ *
+ * \author      Roy QIU <karoyqiu@gmail.com>
+ * \copyright   © 2022 Roy QIU。
+ *
+ **************************************************************************************************/
+#include <iostream>
+#include <asio.hpp>
+
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello!" << std::endl;
+
+    try
+    {
+        asio::io_context ctx;
+        ctx.run();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "EXCEPTION: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
